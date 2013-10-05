4ed321ca78b0072526e3e9cc97c762145257e17e
Sidney <sidneyquitorio8@yahoo.com>
2013-10-05 16:30:55 -0700

    Header finished

---
+++ lib/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TackIT: Homage</title>
+
+<!-- Required header files -->
+<script type="text/javascript" src="bootstrap/js/jquery-1.10.2.js"></script>
+<script src = "../lib/boostrap/dist/js/bootstrap.js"></script>
+<link type ="text/css" rel="stylesheet" href="../lib/bootstrap/dist/css/bootstrap.css"/>
+
+</head>
+<body>
+
+
+<br clear="all">
+
+    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </div><!-- /.navbar -->
+
+<p style="margin-bottom:500px;"></p>
+</center>
+</body>
+</html>
