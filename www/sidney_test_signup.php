835432da640a62e3c221229263995c14109e79f2
Sidney <sidneyquitorio8@yahoo.com>
2013-10-12 18:09:32 -0700

    Signup complete

--- www/sidney_test_signup.php
+++ www/sidney_test_signup.php
@@ -1,12 +1,14 @@
 <?php include "../lib/php/header.php"; ?>
 
+<script src = "../lib/javascript/signup.js"></script>
+
 <br><br><br><br><br>
 
-<form action="demo_form.asp">
-Email: <input type="text" name="email"><br>
-Username: <input type="text" name="username"><br>
-Password: <input type="password" name="password"><br>
-<input class="signup" type="submit" value="Submit">
+<form>
+Email: <input id="signup_email" type="text" name="email"><br>
+Username: <input id="signup_username" type="text" name="username"><br>
+Password: <input id="signup_password" type="password" name="password"><br>
+<input id="signup_button" type="submit" value="Submit">
 </form>
 
 <?php include "../lib/php/footer.php"; ?>
