337348a5de34bc5cf56b93ef83f9c76f212b09c1
Taithere <taithere@yahoo.com>
2013-10-12 17:52:05 -0700

    Added Board function

--- lib/php/tack.php
+++ lib/php/tack.php
@@ -1,4 +1,5 @@
 <?php
+
 class Tack {
 
     const EMPTY_STRING = '';
@@ -8,10 +9,6 @@ class Tack {
     const DB_DESTRIPTION = "description";
     const DB_TACKURL = "tackUrl";
     const DB_IMAGE = "imageURL";
-    
-
-
-
 
     private $id;
     private $user_id;
@@ -22,15 +19,15 @@ class Tack {
     private $imageURL;
     private $creation_time;
 
-    
-    
     public function __construct($user_id, $board_id, $title, $description, $tackURL, $imageURL, $id = self::EMPTY_STRING, $creation_time = self::EMPTY_STRING) {
-        $this->user_id=$user_id;
-        $this->board_id=$board_id;
-        $this->title=$title;
-        $this->description=$description;
-        $this->tackURL=$tackURL;
-        $this->imageURL=$imageURL;
+        $this->user_id = $user_id;
+        $this->board_id = $board_id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->tackURL = $tackURL;
+        $this->imageURL = $imageURL;
+        $this->id= $id;
+        $this->creation_time = $creation_time;
     }
 
     public function set_id($new_id) {
@@ -96,41 +93,41 @@ class Tack {
     public function get_creation_time() {
         return $this->creation_time;
     }
-    
+
     public static function createTack($userId, $boardID, $title, $description, $tackUrl, $imageUrl, $creationTime) {
         $db = new Database();
         $con = $db->getConnection();
-        
+
         // escape inputs
         $userId = $con->real_escape_string($userId);
         $boardID = $con->real_escape_string($boardID);
         $title = $con->real_escape_string($title);
-        $description =  $con->real_escape_string($description);
+        $description = $con->real_escape_string($description);
         $tackUrl = $con->real_escape_string($tackUrl);
         $imageUrl = $con->real_escape_string($imageUrl);
         $creationTime = $con->real_escape_string($creationTime);
 
-       //build transaction
+        //build transaction
         $insertTack = "INSERT INTO `tackit`.`tack` (user_id, board_id, title, description, tackUrl, imageURL)
            VALUES ('$userId', '$boardID', '$title', '$description', '$tackUrl', '$imageUrl')";
 
         //submit query
-        return $db->doQuery($insertTack);       
-        
+        return $db->doQuery($insertTack);
     }
 
-    public static function getTackFromID($id){
-            $db = new Database();
-            $con = $db->getConnection();
-            
-            //escape input
-            $id = $con->real_escape_string($id);
-            if(($result = $db->doQuery("SELECT * FROM tackit.tack WHERE id = '$id'")) && ($row = $result->fetch_assoc())) {
-                return new Tack($row[self::DB_USER], $row[self::DB_BOARD], $row[self::DB_TITLE], $row[self::DB_DESTRIPTION], $row[self::DB_TACKURL], $row[self::DB_IMAGE]);
-            }
-            else
-                return NULL;
+    public static function getTackFromID($id) {
+        $db = new Database();
+        $con = $db->getConnection();
+
+        //escape input
+        $id = $con->real_escape_string($id);
+        if (($result = $db->doQuery("SELECT * FROM tackit.tack WHERE id = '$id'")) && ($row = $result->fetch_assoc())) {
+            return new Tack($row[self::DB_USER], $row[self::DB_BOARD], $row[self::DB_TITLE], $row[self::DB_DESTRIPTION], $row[self::DB_TACKURL], $row[self::DB_IMAGE]);
+        }
+        else
+            return NULL;
     }
-    }
-        
-        ?>
+
+}
+
+?>
--- lib/php/tacktest.php
+++ lib/php/tacktest.php
@@ -9,8 +9,12 @@
 
         //$test->createTack(24,2,"three","four","five","six","seven");
 	
-        var_dump(Tack::getTackFromID(19));
+        //var_dump(Tack::getTackFromID(19));
 	echo 'cake';
-	echo $test->get_id();
- 
+	//echo $test->get_id();
+        //$test = new Board(0,"num","hello",24);
+        //$test->createBoard(26,1,"num","hello");
+        
+         echo 'cake';
+        
 ?>
