df97fd6aba851573895c67a52bf42898c29d1343
Sidney <sidneyquitorio8@yahoo.com>
2013-10-12 17:33:35 -0700

    Finished stub

---
+++ www/sidney_test_signup.php
@@ -0,0 +1,12 @@
+<?php include "../lib/php/header.php"; ?>
+
+<br><br><br><br><br>
+
+<form action="demo_form.asp">
+Email: <input type="text" name="email"><br>
+Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input class="signup" type="submit" value="Submit">
+</form>
+
+<?php include "../lib/php/footer.php"; ?>
