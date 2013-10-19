78578675e52e08b23c7033a7ee3efbbf7a65cb65
David <davidng0123@live.com>
2013-10-19 13:50:00 -0700

    TackitException returns associative array

--- lib/php/TackitException.php
+++ lib/php/TackitException.php
@@ -8,6 +8,8 @@
 class TackitException extends Exception {
 
     const EXCEPTION_HEADER = 'error';
+    const EXCEPTION_CODE = 'code';
+    const EXCEPTION_DESC = 'desc';
 
     public function __construct($message, $code, $previous = NULL) {
         parent::__construct($message, $code, $previous);
@@ -15,9 +17,9 @@ class TackitException extends Exception {
 
     public function getJson() {
         $error = array(
-            self::EXCEPTION_HEADER,
-            parent::getCode(),
-            parent::getMessage()
+            self::EXCEPTION_HEADER => true,
+            self::EXCEPTION_CODE   => parent::getCode(),
+            self::EXCEPTION_DESC   => parent::getMessage()
         );
         return json_encode($error);
     }
