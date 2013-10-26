e64e72eaea9a5a3e3e224f74c078f455dff565e0
David <davidng0123@live.com>
2013-10-26 16:25:25 -0700

    relate.php api template

---
+++ api/relate.php
@@ -0,0 +1,21 @@
+<?php
+try {
+    require_once '../lib/php/Session.php';
+    require_once '../lib/php/User.php';
+    require_once '../lib/php/Tack.php';
+    require_once '../lib/php/Board.php';
+    require_once '../lib/php/Relationship.php';
+    require_once '../lib/php/TackitException.php';
+    require_once '../lib/php/TackitResponse.php';
+
+    //check & validate cookie
+    //obtain userid
+    if (($userid = Session::isLoggedIn($_COOKIE[Session::COOKIE])) === FALSE)
+        throw new TackitException("Session does not exist or has expired!", 0);
+} catch (TackitException $ex) {
+    echo $ex->getJson();
+} catch (Exception $ex) {
+    $exception = new TackitException("We could not fulfill your request!", 0);
+    echo $exception->getJson();
+}
+?>
