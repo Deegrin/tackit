a6bf957fd9e564ac374ffdca9b33b7a85cec3e60
Sidney <sidneyquitorio8@yahoo.com>
2013-10-26 16:54:18 -0700

    Responsive side nav done

--- lib/php/header.php
+++ lib/php/header.php
@@ -17,6 +17,8 @@
         <script src = "../lib/javascript/login.js"></script>
         <script src = "../lib/javascript/logout.js"></script>
         <link href="../lib/css/style.css" rel="stylesheet" type="text/css">
+        <script src = "../lib/javascript/dashboard.js"></script>
+        <link type="text/css" rel="stylesheet" href="../lib/css/board.css">
 
 
     </head>
