3710d21aeda644725e980e02c438fefa6caf67c1
alexgilham <alexander.gilham@gmail.com>
2013-10-26 13:24:09 -0700

    home page fixed

---
+++ www/homepage.php
@@ -0,0 +1,57 @@
+<?php include "../lib/php/header.php" ?>
+
+<link href="../lib/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+<link href="jumbotron.css" rel="stylesheet">
+    
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to TackIT!</h1>
+        <p>Your bookmarks are important to us. We're here to help you rule your bookmarks and to help make browsing the web a more enjoyable experience.</p>
+        <p><a class="btn btn-primary btn-lg" href="javascript:void(0);" onClick="vpb_show_sign_up_box();">Sign Up </a></p>
+      </div>
+    </div>
+
+
+
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing">
+
+  <div class="row">
+    <div class="col-md-4 text-center">
+      <img src="tack.jpg" class="img-thumbnail">
+      <h2>Tack</h2>
+      <p>A tack starts with an image or video you add to TackIT. You can add a tack from a website using the upload an image right from your computer. Any Tack on TackIT can be retacked, and all tacks link back to their source.</p>
+      <p><a class="btn btn-default" href="javascript:void(0);" onClick="vpb_show_sign_up_box();">Start Tacking »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img src="board.jpg" class="img-thumbnail">
+      <h2>Board</h2>
+      <p>A board is where you organize your tacks by topic. You could tack ideas for halloween costumes to your October Month board, for example. Boards can be secret or public, and you can invite other people to tack with you on any of your boards.</p>
+      <p><a class="btn btn-default" href="javascript:void(0);" onClick="vpb_show_sign_up_box();">Start Making Boards »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img src="follow.jpg" class="img-thumbnail">
+      <h2>Follow</h2>
+      <p>When you follow someone, their tacks show up in your TackIT dashboard. You can follow all of someone's boards or just the ones you like best.</p>
+      <br>
+      <br>
+      <p><a class="btn btn-default" href="javascript:void(0);" onClick="vpb_show_sign_up_box();">Start Following »</a></p>
+    </div>
+  </div><!-- /.row -->
+
+
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2013</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+<?php include "../lib/php/footer.php" ?>
