b84e4e13e67b3aae6f91e625afa7bee5fcff4924
alexgilham <alexander.gilham@gmail.com>
2013-11-18 10:05:20 -0800

    all of alexs pages have converted tabs to spaces

--- www/following.php
+++ www/following.php
@@ -3,105 +3,105 @@
 <link type="text/css" rel="stylesheet" href="../lib/css/boardList.css">
 
 <div id="yourFollowing" class="boardList col-sm-8 col-sm-offset-2">
-	
-  	<div class="panel panel-default">
-  		<a href="#test">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    
+    <div class="panel panel-default">
+        <a href="#test">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
-  	<div class="panel panel-default">
-  		<a href="#">
-	  		<div class="panel-body">
-	  			<div class="title">
-	    		BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
-	    		</div>
-	    		<div class="description">
-	    			al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
-	    		</div>
+    <div class="panel panel-default">
+        <a href="#">
+            <div class="panel-body">
+                <div class="title">
+                BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle BoardTitle
+                </div>
+                <div class="description">
+                    al;sdkflskdfjsa l;akjsdfl;kajsdfj;lkas ;lkasdjfl;akjsdflksd fjl;aksdfjlaksj dfl; laksjdflkajsd;flk l;aksjdf
+                </div>
 
-	  		</div>
-  		</a>
-  	</div>
+            </div>
+        </a>
+    </div>
 
 </div>
 
