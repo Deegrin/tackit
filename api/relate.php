1af2606e283f136ced10d8fff7b1ed64be0da1b0
David <davidng0123@live.com>
2013-11-22 19:12:23 -0800

    fixed retack API

--- api/relate.php
+++ api/relate.php
@@ -33,10 +33,8 @@ try {
                 throw new TackitException("Access denied!", 0);
         } else
             throw new TackitException("Board is invalid", 0);
-    }
-
-
-if (isset($_POST['favorite'])) {
+    } // FAVORITE TACK
+    if (isset($_POST['favorite'])) {
         //validate input
         if (!is_numeric($_POST['favorite']) || $_POST['favorite'] < 0)
             throw new TackitException("favorite is invalid", 0);
@@ -44,52 +42,41 @@ if (isset($_POST['favorite'])) {
         //follow board if user authorized
         if (($tack = Tack::getTackFromID($_POST['favorite'])) !== NULL) {
             //if board is public
-        
-                if (Relationship::favoriteTack($_POST['favorite'], $userid) !== FALSE) {
-                    $response = new TackitResponse();
-                    echo $response->getJson();
-                } else
-                    throw new TackitException("We could not favorite the tack!", 0);
-            
+
+            if (Relationship::favoriteTack($_POST['favorite'], $userid) !== FALSE) {
+                $response = new TackitResponse();
+                echo $response->getJson();
+            } else
+                throw new TackitException("We could not favorite the tack!", 0);
         } else
             throw new TackitException("tack is invalid", 0);
-    }
-
-// recent
-
-if (isset($_POST['retackTack'] && $_POST['retackBoard'])) {
+    } // RETACK TACK TO BOARD
+    if (isset($_POST['retackTack']) && isset($_POST['retackBoard'])) {
         //validate input
         if (!is_numeric($_POST['retackTack']) || $_POST['retackTack'] < 0)
             throw new TackitException("retackTack is invalid", 0);
         if (!is_numeric($_POST['retackBoard']) || $_POST['retackBoard'] < 0)
             throw new TackitException("retackBoard is invalid", 0);
 
-            //check if the board exists
-           if (in_array($_POST['retackBoard'], Board::getBoardFromUserID($userid))){
-
-            if (Tack::getTackFromID($_POST['retackTack'])!==NULL){
-                if (Relationship::followBoard($userid, $_POST['board']) !== FALSE) {
+        //check if the board exists
+        if (in_array($_POST['retackBoard'], Board::getBoardFromUserID($userid))) {
+            // check if the tack exists
+            if (Tack::getTackFromID($_POST['retackTack']) !== NULL) {
+                // retack the tack to board
+                if (Tack::retack($userid, $_POST['retackBoard'], $_POST['retackTack']) !== FALSE) {
                     $response = new TackitResponse();
-                    echo $response->getJson();
+                    exit($response->getJson());
                 } else
-                    throw new TackitException("We could not follow the board!", 0);
+                    throw new TackitException("We could not retack the tack!", 0);
             } else
-            throw new TackitException("The tack does not exists", 0);
-
-        }else
-                throw new TackitException("Access denied!", 0);
-
-       
+                throw new TackitException("The tack does not exist!", 0);
+        } else
+            throw new TackitException("Access denied!", 0);
     }
-
-
-
-
-
 } catch (TackitException $ex) {
-    echo $ex->getJson();
+    exit($ex->getJson());
 } catch (Exception $ex) {
     $exception = new TackitException("We could not fulfill your request!", 0);
-    echo $exception->getJson();
+    exit($exception->getJson());
 }
 ?>
