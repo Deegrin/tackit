f9ac2db9b1b73d7d6431983a8fa140c4ca648163
David <davidng0123@live.com>
2013-11-30 17:03:04 -0800

    corrected spelling mistake in header.php

--- lib/php/header.php
+++ lib/php/header.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>TackIT: Homage</title>
+        <title>TackIT: Homepage</title>
 
         <!-- Required header files -->
         <script src="../lib/javascript/facebook_login.js"></script>
