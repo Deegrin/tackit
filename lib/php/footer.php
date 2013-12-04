ec876609ea21d8f6b0f09d7c92c8c13f4c72e2f1
alexgilham <alexander.gilham@gmail.com>
2013-12-04 06:47:46 -0800

    modified footer to contain a link to terms and conditions, also added terms and conditions

--- lib/php/footer.php
+++ lib/php/footer.php
@@ -1,4 +1,13 @@
 
 </body>
 
+<footer>
+<p><center>
+ <a href="../www/terms_and_conditions.php"> Terms and Conditions </a> : 
+ <a href="#"> Contact Us </a> : 
+ <a href="#"> Help </a>
+ 
+ </center></p>
+</footer>
+
 </html>
