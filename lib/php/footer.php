f8a1fd2d6ac110916f494f9fda618b086c88c020
alexgilham <alexander.gilham@gmail.com>
2013-12-04 16:03:43 -0800

    removed footer due to bugs

--- lib/php/footer.php
+++ lib/php/footer.php
@@ -1,13 +1,4 @@
 
 </body>
 
-<footer>
-<p><center>
- <a href="../www/terms_and_conditions.php"> Terms and Conditions </a> : 
- <a href="#"> Contact Us </a> : 
- <a href="#"> Help </a>
- 
- </center></p>
-</footer>
-
 </html>
